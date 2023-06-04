d4dcfc84f89d9249a7a7a9615bff68998bcc1a84
Niladri <niladrisengit12@gmail.com>
2023-06-04 22:04:59 +0530

    part 01

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const parent = React.createElement("div",{},[
     React.createElement("h1",{},"Hello From React1"),
     React.createElement("h2",{},"Hello From React2"),
+    React.createElement("h2",{},"Hello From React2")
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
